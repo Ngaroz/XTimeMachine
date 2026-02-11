60faaabf24b755ca9e1a65c34785fff142e8ad0e
Ngaroz <aelnagar88@yahoo.com>
2026-02-11 06:32:46 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,17 @@
 pluginManagement {
     repositories {
         google()
-        jcenter()
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
-        jcenter()
+        jcenter() // <--- IT MUST BE HERE (The second block)
+        maven { url 'https://api.xposed.info/' } // <--- Add this backup just in case
     }
 }
-allprojects {
-    repositories {
-        google()
-        mavenCentral() // <--- ADD THIS LINE
-        jcenter()
-    }
-}
-rootProject.name = "XTimeMachine"
-include(":app")
